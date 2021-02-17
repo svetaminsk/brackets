8a5f6039d644b5cbfb505530c0b1cf5904f41317
Sviatlana <70862495+svetaminsk@users.noreply.github.com>
2021-02-17 12:28:38 +0300

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+  const result = bracketsConfig.map((bracket) => bracket.join(''));
+    for (let i = 0; i < result.length; i += 1) {
+        if (str.includes(result[i])) {
+            str = str.replace(result[i], '');
+            i = -1;
+        }
+    }
+    return str.length === 0;
+};
